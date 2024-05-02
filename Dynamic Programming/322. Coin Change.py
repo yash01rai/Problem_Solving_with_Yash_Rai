c4a5a739b8da177295112c782c6248782eeb81e9
yash01rai <73843434+yash01rai@users.noreply.github.com>
2024-05-02 11:02:14 +0530

    322. Coin Change

---
+++ Dynamic Programming/322. Coin Change.py
@@ -0,0 +1,36 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        
+        # Bottom Up
+        dp = [inf] * (amount + 1) # will store minimum coins needed to certain amount
+        dp[0] = 0 # 0 means no coins required
+
+        for amt in range(1, amount + 1):
+            for c in coins:
+                    if amt - c >= 0:
+                        # use current coin and get remaining val from dp vs the current amount in dp
+                        dp[amt] = min(dp[amt - c] + 1,  dp[amt])
+        
+        return dp[amount] if dp[amount] != inf else -1
+
+        # Top Down
+        dp = {}
+        def dfs(i, amt):
+
+            if amt in dp:
+                return dp[amt]
+
+            if amt == amount:
+                return 0
+            
+            if amt > amount or i == len(coins):
+                return inf
+
+            dp[amt] = min(1 + dfs(i, amt + coins[i]), dfs(i + 1, amt))
+            return dp[amt]
+
+        min_coins = dfs(0, 0)
+        return min_coins if min_coins != inf else -1
+
+        # Time - O(amt * n)
+        # Space - O(amt)
