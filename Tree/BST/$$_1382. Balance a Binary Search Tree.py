349466b2aa8ba74a845a014c784b96a12a021a8b
yash01rai <73843434+yash01rai@users.noreply.github.com>
2024-06-27 18:06:26 +0530

    added comments

---
+++ Tree/BST/$$_1382. Balance a Binary Search Tree.py
@@ -0,0 +1,50 @@
+# Question Link -> https://leetcode.com/problems/balance-a-binary-search-tree/?envType=daily-question&envId=2024-06-26
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def balanceBST(self, root: TreeNode) -> TreeNode:
+
+        # Topics
+        # Recursion, Depth-First Search (DFS)
+        # In-order traversal:
+        #    1. Explore the left sub-tree
+        #    2. Visit the current node
+        #    3. Explore the right sub-tree
+        # In-order traversal of a binary search tree (BST) helps us retrieve values in non-decreasing order.
+
+        # Approach
+        # We'll get the node values stored in an array using DFS.
+        # Then, using DFS again, we'll create a tree by creating a node with the middle value of the array.
+        # We'll call recursion on both start -> mid - 1 and mid + 1 -> end,
+        # assigning recursive calls to node.left and node.right.
+
+        nodes = []
+
+        def dfs(node):
+            if not node:
+                return
+
+            dfs(node.left)
+            nodes.append(node.val)
+            dfs(node.right)
+        
+        dfs(root)
+
+
+        def dfs(start, end):
+            if start > end:
+                return
+            
+            mid = (start + end) // 2
+            node = TreeNode(nodes[mid])
+            node.left = dfs(start, mid - 1)
+            node.right = dfs(mid + 1, end)
+
+            return node
+        
+        return dfs(0, len(nodes) - 1)
